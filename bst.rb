6cbcc5dbf6f56fb0fc3207b51ab8e9a1a6279905
Ulisses76 <aulissesn@gmail.com>
2023-02-20 16:17:28 -0300

    in the middle of work!

--- bst.rb
+++ bst.rb
@@ -1,14 +1,21 @@
 class Node
-  attr_accessor :node, :left, :rigth
+  attr_accessor :data, :left, :right
 
-  def initialize(d)
-    @data = d
+  def initialize(data)
+    @data = data
     @left = nil
-    @rigth = nil
+    @right = nil
     root = nil
   end
 
 end
+
+  class Tree
+    attr_accessor :node
+
+    def initialize(array)
+      @root = build_tree(array.uniq.sort, 0, array.uniq.length - 1)
+    end
 
   def build_tree(array, start, final)
     return if start > final
@@ -16,15 +23,19 @@ end
     
     node = Node.new(array[mid])
     node.left = build_tree(array, start, mid - 1)
-    node.rigth = build_tree(array, mid + 1, final)
+    node.right = build_tree(array, mid + 1, final)
     node
   end
 
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 
+end
 
-array = [1,2,3,4,5]
+array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-root = build_tree(array, 0, array.length - 1)
-
-p root.left.rigth
-p root.rigth.rigth
+bsb = Tree.new(array)
+p bsb.pretty_print
